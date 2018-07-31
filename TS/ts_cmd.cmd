8319d5deb45ee79d57ecc7949b329fcb379759c9
gerryzhou <gerryztech@gmail.com>
2018-07-31 16:04:13 -0500

    Add cmd functions for GS_Ladders strategy;

---
+++ TS/ts_cmd.cmd
@@ -0,0 +1,17 @@
+﻿Algo_Mode,1
+AlgoMode,0
+ProfitTargetAmt,375
+StopLossAmt,150
+BreakEvenAmt,250
+BarsSincePT,1
+BarsSinceSL,1
+BarsAgoMaxPbSAREn,5
+BarsMaxLastCross,54
+TimeStartH,21
+TimeStartM,59
+TimeEndH,14
+TimeEndM,59
+FlatTimeStart,1505
+FlatTimeEnd,1700
+TradeDirection,1
+TradeStyle,1
